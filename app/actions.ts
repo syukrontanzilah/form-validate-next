d61fa5f65dd1c9be18c37a8140382eb30e9e2f8e
syukrontanzilah <syukrontanzilah@gmail.com>
2024-12-11 19:07:12 +0700

    add schema

--- app/actions.ts
+++ app/actions.ts
@@ -1,16 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-"use server"
+"use server";
 
+import { parseWithZod } from "@conform-to/zod";
 import { redirect } from "next/navigation";
+import { submissionSchema } from "./zodSchema";
 
-export async function TalkToSalesAction(formData: FormData){
-    const response = await fetch(process.env.TALK_TO_SALES_URL!, {
-        method: "POST",
-        body: formData,  
-    });
+export async function TalkToSalesAction(prevState: any, formData: FormData) {
+  const submission = parseWithZod(formData, {
+    schema: submissionSchema,
+  });
 
-    if(!response.ok){
-        throw new Error('Something went wrong')
-    }
-    return redirect("/success")
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const response = await fetch(process.env.TALK_TO_SALES_URL!, {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  return redirect("/success");
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // import Image from "next/image";
+"use client"
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -8,8 +9,25 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Textarea } from "@/components/ui/textarea";
 import { TalkToSalesAction } from "./actions";
 import { SubmitButton } from "./components/SubmitButton";
+// import { useFormState } from "react-dom";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { submissionSchema } from "./zodSchema";
+import { useActionState } from "react";
 
 export default function Home() {
+  const [salesResult, salesAction] = useActionState(TalkToSalesAction, undefined);
+
+  const [salesForm, salesFields] = useForm({
+    lastResult: salesResult,
+
+    onValidate({formData}){
+      return parseWithZod(formData, {schema: submissionSchema} )
+    },
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput',
+  })
+
   return (
     <section className="min-h-screen w-screen flex flex-col items-center justify-center px-5">
       <h1 className="text-4xl font-bold mb-7">Contact us</h1>
@@ -28,25 +46,40 @@ export default function Home() {
                 please contact us down below!
               </div>
               <form 
-              action={TalkToSalesAction}
+              action={salesAction}
+              id={salesForm.id}
+              onSubmit={salesForm.onSubmit}
+              noValidate
               className="flex flex-col gap-y-4 mt-5"
               >
                 <input type="hidden" name="_gotcha"/>
                 <div className="grid space-y-1">
                   <Label>Name</Label>
-                  <Input type="text" name="name" placeholder="Input Name" />
+                  <Input name={salesFields.name.name} 
+                  defaultValue={salesFields.name.initialValue}
+                  key={salesFields.name.key} placeholder="Name" />
+                  <p className="text-red-500 text-sm">{salesFields.name.errors}</p>
                 </div>
+
                 <div className="grid space-y-1">
                   <Label>Email</Label>
-                  <Input type="email" name="email" placeholder="Email" />
+                  <Input 
+                  name={salesFields.email.name}
+                  key={salesFields.email.key}
+                  defaultValue={salesFields.email.initialValue}
+                  placeholder="Email" />
+                  <p className="text-red-500 text-sm">{salesFields.email.errors}</p>
                 </div>
                 <div className="grid space-y-2">
                   <Label>Question or problem</Label>
                   <Textarea
-                    name="message"
+                    name={salesFields.message.name}
+                    key={salesFields.message.key}
+                    defaultValue={salesFields.message.initialValue}
                     className="h-32"
                     placeholder="Please share some details about your needs.."
                   />
+                  <p className="text-red-500 text-sm">{salesFields.message.errors}</p>
                 </div>
                 {/* <Button type="submit">Submit</Button> */}
                 <SubmitButton/>
